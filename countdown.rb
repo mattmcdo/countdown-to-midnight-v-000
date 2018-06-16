63cd1358b5337758be527eb684c2562200840a90
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-16 21:13:16 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,1 @@
 #write your code here
-n = 10
-def countdown(n)
-while n>0
-    puts "#{n} SECOND(S)!"
-    n -=1
-  end
-"HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(count)
-  while count > 0
-    count-=1
-    sleep(5)
-  end
-end
