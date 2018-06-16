37e9f689e5c59da458b0485f4df6bf75a00eae48
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-16 21:28:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,11 @@ end
 return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(n)
-  sleep(5)
+def countdown_with_sleep(n,x)
+  sleep(x)
   until n == 0
   puts "#{n} SECOND(S)!"
   n -= 1
   end
   return "HAPPY NEW YEAR!"
-
 end
