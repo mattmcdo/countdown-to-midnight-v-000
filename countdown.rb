e8d6f608cd08069d54d0e225008ccc4d407c4d6c
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-16 21:33:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,1 +1,17 @@
 #write your code here
+def countdown(n)
+until n == 0
+puts "#{n} SECOND(S)!"
+n -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  until n == 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  end
+  sleep(5)
+return "HAPPY NEW YEAR!"
+end
